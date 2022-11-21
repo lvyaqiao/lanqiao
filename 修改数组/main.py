e35903034ad2f24a8f29e7a23a85e45b202d9f2a
lvyaqiao <lvyaqiao@126.com>
2022-11-21 22:06:34 +0800

    添加了砝码称重

---
+++ 修改数组/main.py
@@ -0,0 +1,24 @@
+def get_input():
+    N = int(input())
+    lst = [int(i) for i in input().split()]
+    return N, lst
+
+def solution():
+    N, lst = get_input()
+    dic = {}
+    def dic_found(number):
+        father = dic.get(number,False)
+        if father == False:
+            dic[number] = dic.get(number + 1 ,number + 1)
+            return number
+        else:
+            result = dic_found(father)
+            dic[number] = result
+        return result
+
+    for i in range(len(lst)):
+        lst[i] = dic_found(lst[i])
+    for i in lst:
+        print(i,end = " ")
+  
+solution()
